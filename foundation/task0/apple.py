100333c65c75d2f83ea8af78c30ab3587d7bd9ff
isguyd <239635394@qq.com>
2026-09-21 22:51:35 +0800

    传错了

--- foundation/task0/apple.py
+++
@@ -1,8 +0,0 @@
-aList = []
-ans = 0
-aList=(list (map(int,input().split())))
-height = int(input())+30
-for i in range(10):
-    if height >= aList[i]:
-       ans+=1
-print(ans) 
--- foundation/task0/素数.py
+++
@@ -1,8 +0,0 @@
-a=int(input())
-b=int(a**0.5)
-for i in range(2,b):
-    if a%i==0:
-        print("NO")
-        break
-        sys.exit(0)
-print("YES")
